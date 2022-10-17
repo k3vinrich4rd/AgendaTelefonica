741d7ed5835dd3f12d1a87a1de8e1eac4339901f
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-17 01:33:19 -0300

    Inserindo mudanças de identação

--- src/main/java/Agenda/Telef/nica/contatosAgenda/model/ContatosModel.java
+++ src/main/java/Agenda/Telef/nica/contatosAgenda/model/ContatosModel.java
@@ -1,6 +1,8 @@
 package Agenda.Telef.nica.contatosAgenda.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.UUID;
